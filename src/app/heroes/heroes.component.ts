c2820f2f63358973cea9ee0dadc2a8dd7c7e7b25
Alejandro <cursomeancore@gmail.com>
2020-03-11 19:19:32 +0100

    Acabado sección 3

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from './../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,13 +9,24 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
 
   // constructor de la clase
   constructor() { }
 
   // constructor del componente
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
