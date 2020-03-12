b3bec6b5d98c7acd148a143c20b31bfd52e6ca92
Alejandro <cursomeancore@gmail.com>
2020-03-12 19:30:15 +0100

    Hasta el componente de mensajes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,45 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
+
+/*
+// creación de observable
+const observable = new Observable<string>((observer) => {
+  observer.next('hola');
+  observer.next('hola');
+  //observer.error('Se ha producido un error');
+
+  setInterval(() => {
+    observer.next('hola');
+  }, 1000);
+
+  observer.next('hola');
+  observer.complete();
+  // observer.error('hola');
+});
+
+// uso del observable
+const suscriptor = observable.subscribe((datos: string) => {
+// next
+
+  console.log(datos);
+}, (err) => {
+// error
+  console.log(err);
+  
+
+} , () => {
+  // complete
+  console.log('Ya no ha más datos que recibir');
+});
+
+setTimeout(() => {
+  suscriptor.unsubscribe();
+  
+}, 5000);
+
+*/
